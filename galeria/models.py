6e1ca5593154376594e4b26eecf23c521e66c403
Gustavo Figueiredo <gustavopfp.trab@gmail.com>
2023-02-08 11:39:09 -0300

    Fotografias model created and already make the migrartions

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Fotografias(models.Model): #nome legenda descrição foto categoria data de criação
+    OPCOES_CATEGORIAS = (
+        ("NEBULOSA", "Nebulosa"),
+        ("ESTRELA", "Estrela"),
+        ("GALÁXIA", "Galáxia"),
+        ("PLANETA", "Planeta"),
+    )
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    legenda = models.CharField(max_length=100, null=False, blank=False)
+    descricao = models.TextField(max_length=250, null=False, blank=False)
+    categoria = models.CharField(max_length=12, choices=OPCOES_CATEGORIAS, default="")
+    data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
+    imagem = models.ImageField(upload_to="fotos/%Y/%m/%d", blank=True)
+
+    def __str__(self) -> str:
+        return  self.nome
